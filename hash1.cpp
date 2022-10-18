778c4169d6d56e8267c28c564d12736f5c07475f
chrisioan <christos_00@hotmail.co.uk>
2022-10-19 00:19:22 +0300

    fixed issue with hash1

--- hash1.cpp
+++ hash1.cpp
@@ -7,7 +7,8 @@
 int32_t hash1(int32_t key, uint32_t n)
 {
     uint32_t num = 1;
-    num <<= n + 1;
+    num <<= n;
+    // e.g. n = 3
     // 1000 - 1 = 111
     // val = key & (2^n - 1); // bitwise AND
     return key & (num - 1);
