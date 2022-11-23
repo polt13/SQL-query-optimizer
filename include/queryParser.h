184561ece343287278183a39a106bdd9a047fd07
polt13 <poltamtamis@gmail.com>
2022-11-23 23:33:50 +0200

    working on predicates

--- include/queryParser.h
+++ include/queryParser.h
@@ -5,11 +5,22 @@
 
 constexpr size_t relation_count = 14;
 
+enum class operators { EQ, GREATER, LESS };
+
+struct operations {
+  // store literal value OR a.b (relation.column) in string form
+  char* left;
+  operators op;
+  char* right;
+};
+
 class QueryParser {
   size_t relations_index[relation_count];
   simple_vector<char*> parse_names(char*);
+  simple_vector<operations> parse_predicates(char*);
 
  public:
   void parse_query(char*);
 };
+
 #endif
--- queryParser.cpp
+++ queryParser.cpp
@@ -8,8 +8,8 @@ void QueryParser::parse_query(char* query) {
   char* buffr;
   char* used_relations = strtok_r(query, "|", &buffr);
   simple_vector<char*> names = parse_names(used_relations);
-  for (long i = 0; i < names.getSize(); i++) printf("%s\n", names[i]);
   char* predicates = strtok_r(nullptr, "|", &buffr);
+  simple_vector<operations> filters = parse_predicates(predicates);
   // buffr now points to the last part of the query
   char* selections = buffr;
 }
@@ -19,7 +19,29 @@ simple_vector<char*> QueryParser::parse_names(char* rel_string) {
   char* buffr;
   char* rel = strtok_r(rel_string, " ", &buffr);
   rel_names.add_back(rel);
-  while (rel = strtok_r(nullptr, " ", &buffr)) rel_names.add_back(rel);
+  while ((rel = strtok_r(nullptr, " ", &buffr))) rel_names.add_back(rel);
 
   return rel_names;
+}
+
+simple_vector<operations> QueryParser::parse_predicates(char* predicates) {
+  char* buffr;
+  char* buffr2;
+  char* first_predicate = strtok_r(predicates, "&", &buffr);
+  // split based on the operator
+  char operator[2];
+  operators operation_type;
+  // determine predicate type
+  if (std::strchr(first_predicate, '=')) {
+    operation_type = operators::EQ;
+    std::strcpy(operator, "=");
+  } else if (std::strchr(first_predicate, '>')) {
+    operation_type = operators::GREATER;
+    std::strcpy(operator, ">");
+  } else {
+    operation_type = operators::LESS;
+    std::strcpy(operator, "<");
+  }
+
+  return simple_vector<operations>();
 }
